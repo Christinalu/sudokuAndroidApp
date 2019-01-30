f887c38f73b792d96f092acf23a61aa0c6fb88ba
Elmira <eazizika@sfu.ca>
2019-01-29 19:51:17 -0800

    initial commit

---
+++ app/src/main/java/com/omicron/android/cmpt276_1191e1_omicron/MainActivity.java
@@ -0,0 +1,13 @@
+package com.omicron.android.cmpt276_1191e1_omicron;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
