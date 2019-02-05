68e1b48ba6be67247f3bb5450260bb71689dcc29
w <w>
2019-02-05 15:25:04 -0800

    proto

--- app/src/main/java/com/omicron/android/cmpt276_1191e1_omicron/GameActivity.java
+++ app/src/main/java/com/omicron/android/cmpt276_1191e1_omicron/GameActivity.java
@@ -47,6 +47,7 @@ public class GameActivity extends AppCompatActivity
 	public ColourSqr sqrColour;
 	public float sqrLO;
 	public float sqrTO;
+	public ButtonLayout buttonMatrix;
 
 	public float txtLO;
 	public float txtTO;
@@ -95,7 +96,7 @@ public class GameActivity extends AppCompatActivity
 
 		////// test/////
 
-		Button btnTest = (Button) findViewById(R.id.button_test);
+		/*Button btnTest = (Button) findViewById(R.id.button_test);
 
 		btnTest.setOnClickListener(new View.OnClickListener() {
 											 @Override
@@ -111,7 +112,7 @@ public class GameActivity extends AppCompatActivity
 												 //startActivity(activityDictionary); //switch to dictionary window
 											 }
 										 }
-		);
+		);*/
 
 
 		/////////////
@@ -204,6 +205,94 @@ public class GameActivity extends AppCompatActivity
 				canvas.drawRect( rectArr[i][j], paint );
 			}
 		}
+
+			/** SET BUTTON LISTENERS **/
+
+		Button btn1 = (Button) findViewById(R.id.keypad_1);
+		btn1.setOnClickListener(new View.OnClickListener() {
+									   @Override
+									   public void onClick(View v) {
+										   Toast.makeText(GameActivity.this, "BTN-1", Toast.LENGTH_SHORT).show();
+									   }
+								   }
+		);
+
+		Button btn2 = (Button) findViewById(R.id.keypad_2);
+		btn2.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-2", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+		Button btn3 = (Button) findViewById(R.id.keypad_3);
+		btn3.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-3", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+		Button btn4 = (Button) findViewById(R.id.keypad_4);
+		btn4.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-4", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+		Button btn5 = (Button) findViewById(R.id.keypad_5);
+		btn5.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-5", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+		Button btn6 = (Button) findViewById(R.id.keypad_6);
+		btn6.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-6", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+		Button btn7 = (Button) findViewById(R.id.keypad_7);
+		btn7.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-7", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+		Button btn8 = (Button) findViewById(R.id.keypad_8);
+		btn8.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-8", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+		Button btn9 = (Button) findViewById(R.id.keypad_9);
+		btn9.setOnClickListener(new View.OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										Toast.makeText(GameActivity.this, "BTN-9", Toast.LENGTH_SHORT).show();
+									}
+								}
+		);
+
+
+		//buttonMatrix.create( GameActivity, btn1  );
+
+
 
 
 		/** DRAW TEXT BUTTONS **/
