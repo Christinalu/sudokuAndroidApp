e616304f8e371b45671861094ea9ef073315ce4e
w <w>
2019-03-20 00:46:53 -0700

    minor adjustment

--- app/src/main/java/com/omicron/android/cmpt276_1191e1_omicron/SudokuGenerator.java
+++ app/src/main/java/com/omicron/android/cmpt276_1191e1_omicron/SudokuGenerator.java
@@ -66,9 +66,9 @@ public class SudokuGenerator implements Serializable {
         
         // TODO: remove the following once implementation complete
 	
-		copyarr( testSeed9x9, PuzzleOriginal );
-		copyarr( testSeed9x9, Puzzle );
-		copyarr( testSeed9x9Solution, PuzzleSol );
+		copyarr( PuzzSeedEasy, PuzzleOriginal );
+		copyarr( PuzzSeedEasy, Puzzle );
+		copyarr( PuzzSeedEasySol, PuzzleSol );
     
         // TODO: enable scramble() and copyarr()
         
