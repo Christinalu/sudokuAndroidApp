938eb3acadb9e4627907d4ebe784cc8396dc9e58
Elmira <eazizika@sfu.ca>
2019-04-01 20:51:46 -0700

    appreance of buttons while holding is fixed double click problem on the main menu is fixed wordarray test is added

---
+++ app/src/test/java/com/omicron/android/cmpt276_1191e1_omicron/WordArrayTest.java
@@ -0,0 +1,128 @@
+package com.omicron.android.cmpt276_1191e1_omicron;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordArrayTest {
+
+
+    private WordArray mWordArray1 = new WordArray(1,2,3);
+
+
+
+    @Test
+    public void describeContents() {
+
+    }
+
+    @Test
+    public void writeToParcel() {
+    }
+
+    @Test
+    public void setUpBasedOnUserTypePreference() {
+    }
+
+    @Test
+    public void getNativeLang() {
+    }
+
+    @Test
+    public void getTranslationLang() {
+    }
+
+    @Test
+    public void getPackageName() {
+    }
+
+    @Test
+    public void initializeWordArray() {
+    }
+
+    @Test
+    public void getUsrPuzzleTypePref() throws Exception{
+        assertEquals(1, mWordArray1.getUsrPuzzleTypePref());
+        assertNotEquals(2, mWordArray1.getUsrPuzzleTypePref());
+    }
+
+    @Test
+    public void getWordArray() {
+    }
+
+    @Test
+    public void getWordCount() {
+    }
+
+    @Test
+    public void getSize4x4() {
+    }
+
+    @Test
+    public void getSize6x6() {
+    }
+
+    @Test
+    public void getSize9x9() {
+    }
+
+    @Test
+    public void getSize12x12() {
+    }
+
+    @Test
+    public void getWordNativeAtIndex() {
+    }
+
+    @Test
+    public void getWordTranslationAtIndex() {
+    }
+
+    @Test
+    public void setWordNativeAtIndex() {
+    }
+
+    @Test
+    public void setWordTranslationAtIndex() {
+    }
+
+    @Test
+    public void getWordInFileLineNumAtIndex() {
+    }
+
+    @Test
+    public void getWordHintClickAtIndex() {
+    }
+
+    @Test
+    public void wordUpdateHintClickAtIndex() {
+    }
+
+    @Test
+    public void wordIncrementHintClickAtIndex() {
+    }
+
+    @Test
+    public void getWordAlreadyUsedInGameAtIndex() {
+    }
+
+    @Test
+    public void setWordUsedInGameAtIndex() {
+    }
+
+    @Test
+    public void getWordAllowToDecreaseDifficultyAtIndex() {
+    }
+
+    @Test
+    public void setWordToAllowToDecreaseDifficultyAtIndex() {
+    }
+
+    @Test
+    public void setWordDoNotAllowToDecreaseDifficultyAtIndex() {
+    }
+
+    @Test
+    public void selectWord() {
+    }
+}
