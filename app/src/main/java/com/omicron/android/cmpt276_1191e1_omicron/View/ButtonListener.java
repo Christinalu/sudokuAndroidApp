bd975fe79dad3cd331006cea81bfb398ba209b96
Elmira <eazizika@sfu.ca>
2019-04-08 16:27:49 -0700

    1. now the hint will be shown as long as user hold the button actions to be done : a. have to check if puzzle is correct (only when allowed by efficiency) and if true, disable buttonListener b. the appearance of button needs to be changed while pressing it
    /

--- app/src/main/java/com/omicron/android/cmpt276_1191e1_omicron/View/ButtonListener.java
+++ app/src/main/java/com/omicron/android/cmpt276_1191e1_omicron/View/ButtonListener.java
@@ -6,7 +6,9 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -22,8 +24,7 @@ import com.omicron.android.cmpt276_1191e1_omicron.View.drw;
 import com.omicron.android.cmpt276_1191e1_omicron.WordArray;
 
 
-public class ButtonListener extends AppCompatActivity
-{
+public class ButtonListener extends AppCompatActivity {
 	/*
 	 *	This class is used to set up keypad buttons for GameActivity
 	 *	This also contains code that will let buttons respond
@@ -31,16 +32,16 @@ public class ButtonListener extends AppCompatActivity
 	 */
 
 	private int i;
-	private Button [] btnArr;
+	private Button[] btnArr;
+	private boolean flag_tap; //
 
-	
-	
+
 	public ButtonListener(final Pair currentRectColoured, final SudokuGenerator usrSudokuArr, final drw drawR,
 						  final int[] touchX, final int[] touchY, final Pair lastRectColoured,
 						  final int usrLangPref, final int[] btnClicked, final TextView Hint, final WordArray wordArray,
 						  final int usrModePref, final String[] numArray, int WORD_COUNT, int COL_PER_BLOCK,
-						  int ROW_PER_BLOCK, Context context, TableLayout tableLayout, int orientation, int state,
-						  int[] orderArr, final int[] rotation, final int[] undoBtnPressed ) {
+						  int ROW_PER_BLOCK, final Context context, TableLayout tableLayout, int orientation, int state,
+						  int[] orderArr, final int[] rotation, final int[] undoBtnPressed) {
 		// pulled out of button listeners
 		final Handler handler = new Handler();
 		int rowCount;
@@ -52,24 +53,32 @@ public class ButtonListener extends AppCompatActivity
 
 		btnArr = new Button[WORD_COUNT]; // set buttons as an array
 
-		Log.d( "listener", "WORD_COUNT: " + WORD_COUNT );
-		if( btnArr == null){ Log.d( "listener", "ERROR: null Button[]" ); }
+		Log.d("listener", "WORD_COUNT: " + WORD_COUNT);
+		if (btnArr == null) {
+			Log.d("listener", "ERROR: null Button[]");
+		}
 
-			/* CREATE TABLE LAYOUT */
-		if( COL_PER_BLOCK > ROW_PER_BLOCK ) //set row count as biggest of ROW/COL_PER_BLOCK, so that less buttons per row
-		{ rowCount = COL_PER_BLOCK; btnCount = ROW_PER_BLOCK; }		// but more rows allows to fit bigger words
-		else{ rowCount = ROW_PER_BLOCK; btnCount = COL_PER_BLOCK; }
+		/* CREATE TABLE LAYOUT */
+		if (COL_PER_BLOCK > ROW_PER_BLOCK) //set row count as biggest of ROW/COL_PER_BLOCK, so that less buttons per row
+		{
+			rowCount = COL_PER_BLOCK;
+			btnCount = ROW_PER_BLOCK;
+		}        // but more rows allows to fit bigger words
+		else {
+			rowCount = ROW_PER_BLOCK;
+			btnCount = COL_PER_BLOCK;
+		}
 
 
 		TableRow tableRow;
 		Button button;
 
-		Log.d( "listener", "rowCount: " + rowCount );
-		Log.d( "listener", "btnCount: " + btnCount );
+		Log.d("listener", "rowCount: " + rowCount);
+		Log.d("listener", "btnCount: " + btnCount);
 
 		// if it's in PORTRAIT mode
-		if(orientation == 1) {
-			for( int j=0; j<rowCount; j++ ) //add button rows to table
+		if (orientation == 1) {
+			for (int j = 0; j < rowCount; j++) //add button rows to table
 			{
 				tableRow = new TableRow(context);
 				for (int k = 0; k < btnCount; k++) {
@@ -89,10 +98,10 @@ public class ButtonListener extends AppCompatActivity
 						if (usrLangPref == 0) {
 							button.setText(wordArray.getWordNativeAtIndex(indexArr));
 						} else {
-							button.setText(wordArray.getWordTranslationAtIndex(indexArr));;
+							button.setText(wordArray.getWordTranslationAtIndex(indexArr));
+							;
 						}
-					}
-					else {
+					} else {
 						//user is doing listening comprehension
 						if (usrLangPref == 0) {
 							button.setText(wordArray.getWordNativeAtIndex(orderArr[indexArr]));
@@ -109,8 +118,8 @@ public class ButtonListener extends AppCompatActivity
 			}
 		}
 		// if it's in LANDSCAPE mode
-		else if (orientation == 2){
-			TableLayout.LayoutParams params = new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT,1.0f);
+		else if (orientation == 2) {
+			TableLayout.LayoutParams params = new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f);
 			for (int k = 0; k < WORD_COUNT; k++) {
 				button = new Button(context);
 				tableLayout.setOrientation(TableLayout.VERTICAL);
@@ -125,10 +134,10 @@ public class ButtonListener extends AppCompatActivity
 					if (usrLangPref == 0) {
 						button.setText(wordArray.getWordNativeAtIndex(indexArr));
 					} else {
-						button.setText(wordArray.getWordTranslationAtIndex(indexArr));;
+						button.setText(wordArray.getWordTranslationAtIndex(indexArr));
+						;
 					}
-				}
-				else {
+				} else {
 					//user is doing listening comprehension
 					if (usrLangPref == 0) {
 						button.setText(wordArray.getWordNativeAtIndex(orderArr[indexArr]));
@@ -141,7 +150,7 @@ public class ButtonListener extends AppCompatActivity
 				button.setBackgroundResource(R.drawable.keypad_button);
 				button.setLayoutParams(params);
 				button.setGravity(Gravity.CENTER);
-				button.setPadding(20,0,20,0);
+				button.setPadding(20, 0, 20, 0);
 				button.setSingleLine();
 
 				Log.d("listener", "sample word from array: " + wordArray.getWordNativeAtIndex(indexArr));
@@ -153,7 +162,7 @@ public class ButtonListener extends AppCompatActivity
 
 		}
 
-			/** SET BUTTON LISTENERS **/
+		/** SET BUTTON LISTENERS **/
 
 		// loop to set up all keypad buttons
 		if (state != 2) {
@@ -161,142 +170,152 @@ public class ButtonListener extends AppCompatActivity
 				final int var;
 				if (usrModePref == 0) {
 					var = i + 1;
-				}
-				else {
+				} else {
 					var = orderArr[i] + 1;
 				}
 				final int index;
 				index = i;
-				btnArr[i].setOnLongClickListener(new View.OnLongClickListener() {
-													 @SuppressLint("SetTextI18n")
-													 @Override
-													 public boolean onLongClick(View v) {
-														 Log.d("selectW", "long button press");
-														 Hint.setBackgroundColor(R.drawable.buttons);
-														 if (usrLangPref == 0) {
-															 if (usrModePref == 1) {
-																 Hint.setText(wordArray.getWordNativeAtIndex(index) + " : " + numArray[index]);
-															 } else {
-																 Hint.setText(wordArray.getWordNativeAtIndex(index) + " : " + wordArray.getWordTranslationAtIndex(index));
-															 }
-														 } else {
-															 if (usrModePref == 1) {
-																 Hint.setText(wordArray.getWordTranslationAtIndex(index) + " : " + numArray[index]);
-															 } else {
-																 Hint.setText(wordArray.getWordTranslationAtIndex(index) + " : " + wordArray.getWordNativeAtIndex(index));
-															 }
-														 }
-														 wordArray.wordIncrementHintClickAtIndex(index);
+				btnArr[i].setOnTouchListener(new View.OnTouchListener() {
+						private GestureDetector gestureDetector = new GestureDetector(context , new GestureDetector.SimpleOnGestureListener() {
+							@Override
+							public boolean onDoubleTap(MotionEvent e) {
+								Log.d("TEST", "onDoubleTap");
+								return super.onDoubleTap(e);
+							}
+							@Override
+							public boolean onDown(MotionEvent event) {
+								Log.d("TAG","onDown: ");
 
-														 wordArray.setWordUsedInGameAtIndex(index); //mark as used
-														 wordArray.setWordDoNotAllowToDecreaseDifficultyAtIndex(index); //allow for difficulty to be decreased
-
-														 handler.postDelayed(new Runnable() {
-															 @Override
-															 public void run() {
-																 // Do something after 5s = 5000ms
-																 Hint.setBackgroundColor(Color.TRANSPARENT);
-																 Hint.setText("");
-
-															 }
-														 }, 4000);
+								// don't return false here or else none of the other
+								// gestures will work
+								return super.onDown(event);
+							}
+							@Override
+							public boolean onSingleTapConfirmed(MotionEvent e) {
+								Log.i("TAG", "onSingleTapConfirmed: ");
 
 
-														 return true;
-													 }
-												 }
+								int row = currentRectColoured.getRow();
+								int col = currentRectColoured.getColumn();
+
+								Log.d( "highlight", "btn pressed..." );
+
+								if (row != -1 && usrSudokuArr.PuzzleOriginal[row][col] == 0) {
+									// increase the count of inserted numbers if needed
+									usrSudokuArr.track(currentRectColoured); //important, 'track' must occur before 'usrSudokuArr.Puzzle[][] = x'
+
+									//remove duplicates from (x,y) in preparation of new input and new duplication check
+									usrSudokuArr.removeDuplicates(row,col);
+
+									//if new value is not equal to the old one add last value and its coordinate to history before changing it
+									if (usrSudokuArr.getPuzzle()[row][col] != var) {
+										usrSudokuArr.addHistroy(row, col);
+										usrSudokuArr.printHistory();
+									}
+
+									if( rotation[0] == 0 && undoBtnPressed[0] == 0 ){ //if rotate or pressed 'undo', skip setDrawParameter
+										Log.d( "highlight", "setDrawParameters called from ButtonListener" );
+										drawR.setDrawParameters(touchX, touchY, lastRectColoured, currentRectColoured);
+									}
+
+									// set the cell in the Puzzle to corresponding number based on button user input
+									//if( zoomButtonDisableUpdate[0] == 0 ) // do not update entry when switching modes - causes errors
+									usrSudokuArr.Puzzle[row][col] = var;
+
+									// redraw square matrix and text overlay
+									btnClicked[0] = 1; //this flag allows (for efficiency) class drw to update TextView as well in zoom mode
+
+									if( undoBtnPressed[0] == 1 ){ //
+										//if user has 'undone' some some moves, when inserting a new entry, do not call drw.setDrawParameters because it resets currentRectColoured
+									}
 
 
-				);
-				btnArr[i].setOnClickListener(new View.OnClickListener() {
-												 @Override
-												 public void onClick(View v) {
-													 //if current button selected is valid and is not restricted
-													 int row = currentRectColoured.getRow();
-													 int col = currentRectColoured.getColumn();
+									//if( rotation[0] == 1 )
+									//{ rotation[0] = 0; } //disable rotation flag after user inserted cell
 
-													 Log.d( "highlight", "btn pressed..." );
+									// check if there is a duplicate in row/col/section. MAKE SURE TO HAVE AFTER PUZZLE INPUT IS SET
+									int currentSelectedisCorrect = 0;
+									if (usrSudokuArr.checkDuplicate(row, col)) {
+										Log.d("TESTI", "Duplicate in given coordinate detected");
+										currentSelectedisCorrect = 2;
+									}
+									else {
+										Log.d("TESTI", "No duplicate detected");
+										currentSelectedisCorrect = 1;
+									}
+									drawR.reDraw(currentRectColoured, usrLangPref, currentSelectedisCorrect);
 
-													 if (row != -1 && usrSudokuArr.PuzzleOriginal[row][col] == 0) {
-														 // increase the count of inserted numbers if needed
-														 usrSudokuArr.track(currentRectColoured); //important, 'track' must occur before 'usrSudokuArr.Puzzle[][] = x'
+									btnClicked[0] = 0;
+									//textOverlay.reDrawText( usrLangPref );
 
-														 //remove duplicates from (x,y) in preparation of new input and new duplication check
-														 usrSudokuArr.removeDuplicates(row,col);
+									Log.d("highlight", "btn clicked: " + var);
 
-														 //if new value is not equal to the old one add last value and its coordinate to history before changing it
-														 if (usrSudokuArr.getPuzzle()[row][col] != var) {
-															 usrSudokuArr.addHistroy(row, col);
-															 usrSudokuArr.printHistory();
-														 }
+									//check if word inserted is correct (used to decrease probability of word being selected in WordArray.selectWord() )
+									if (var == usrSudokuArr.getSolution()[row][col]) //if input matches solution
+									{
+										Log.d("selectW", "btn listener: user sqr input correct");
+										if (wordArray.getWordAlreadyUsedInGameAtIndex(var - 1) == false) //if correctly using this word for the first time in game
+										{
+											Log.d("selectW", "btn listener: decrease difficulty");
 
-														 if( rotation[0] == 0 && undoBtnPressed[0] == 0 ){ //if rotate or pressed 'undo', skip setDrawParameter
-															Log.d( "highlight", "setDrawParameters called from ButtonListener" );
-															drawR.setDrawParameters(touchX, touchY, lastRectColoured, currentRectColoured);
-														 }
+											wordArray.setWordUsedInGameAtIndex(var - 1); //mark as used
+											wordArray.setWordToAllowToDecreaseDifficultyAtIndex(var - 1); //allow for difficulty to be decreased
+										}
+									}
+									// do not include "else if inserted wrong input, do not allow to be decreased" because user is likely to make mistakes
+									// SO FAR keep the idea that "if inserted correct word once without using HintClick, it implies the user is getting better with that word"
 
-														 // set the cell in the Puzzle to corresponding number based on button user input
-														 //if( zoomButtonDisableUpdate[0] == 0 ) // do not update entry when switching modes - causes errors
-														 usrSudokuArr.Puzzle[row][col] = var;
-
-														 // redraw square matrix and text overlay
-														 btnClicked[0] = 1; //this flag allows (for efficiency) class drw to update TextView as well in zoom mode
-
-														 if( undoBtnPressed[0] == 1 ){ //
-															//if user has 'undone' some some moves, when inserting a new entry, do not call drw.setDrawParameters because it resets currentRectColoured
-														 }
+									//have to check if puzzle is correct (only when allowed by efficiency) and if true, disable buttonListener
+									/*if (usrSudokuArr.canCheck()) {
+										usrSudokuArr.checkPuzzle(v, btnArr);
+									}*/
 
 
-														 //if( rotation[0] == 1 )
-														 //{ rotation[0] = 0; } //disable rotation flag after user inserted cell
+									Log.d( "highlight", "marked rectArr (in ButtonListener) as selected at: " + currentRectColoured.getRow() + ", " + currentRectColoured.getColumn() );
 
-														 // check if there is a duplicate in row/col/section. MAKE SURE TO HAVE AFTER PUZZLE INPUT IS SET
-														 int currentSelectedisCorrect = 0;
-														 if (usrSudokuArr.checkDuplicate(row, col)) {
-															 Log.d("TESTI", "Duplicate in given coordinate detected");
-															 currentSelectedisCorrect = 2;
-														 }
-														 else {
-															 Log.d("TESTI", "No duplicate detected");
-															 currentSelectedisCorrect = 1;
-														 }
-														 drawR.reDraw(currentRectColoured, usrLangPref, currentSelectedisCorrect);
+								}
 
-														 btnClicked[0] = 0;
-														 //textOverlay.reDrawText( usrLangPref );
+								//debug
+								usrSudokuArr.printCurrent( );
 
-														 Log.d("highlight", "btn clicked: " + var);
+								return super.onSingleTapConfirmed(e);
+							}
+							@Override
+							public void onLongPress(MotionEvent e) {
 
-														 //check if word inserted is correct (used to decrease probability of word being selected in WordArray.selectWord() )
-														 if (var == usrSudokuArr.getSolution()[row][col]) //if input matches solution
-														 {
-															 Log.d("selectW", "btn listener: user sqr input correct");
-															 if (wordArray.getWordAlreadyUsedInGameAtIndex(var - 1) == false) //if correctly using this word for the first time in game
-															 {
-																 Log.d("selectW", "btn listener: decrease difficulty");
+								Log.i("TAG", "onLongPress: ");
+								Hint.setBackgroundColor(R.drawable.buttons);
+								if (usrLangPref == 0) {
+									if (usrModePref == 1) {
+										Hint.setText(wordArray.getWordNativeAtIndex(index) + " : " + numArray[index]);
+									} else {
+										Hint.setText(wordArray.getWordNativeAtIndex(index) + " : " + wordArray.getWordTranslationAtIndex(index));
+									}
+								} else {
+									if (usrModePref == 1) {
+										Hint.setText(wordArray.getWordTranslationAtIndex(index) + " : " + numArray[index]);
+									} else {
+										Hint.setText(wordArray.getWordTranslationAtIndex(index) + " : " + wordArray.getWordNativeAtIndex(index));
+									}
+								}
+								wordArray.wordIncrementHintClickAtIndex(index);
 
-																 wordArray.setWordUsedInGameAtIndex(var - 1); //mark as used
-																 wordArray.setWordToAllowToDecreaseDifficultyAtIndex(var - 1); //allow for difficulty to be decreased
-															 }
-														 }
-														 // do not include "else if inserted wrong input, do not allow to be decreased" because user is likely to make mistakes
-														 // SO FAR keep the idea that "if inserted correct word once without using HintClick, it implies the user is getting better with that word"
+								wordArray.setWordUsedInGameAtIndex(index); //mark as used
+								wordArray.setWordDoNotAllowToDecreaseDifficultyAtIndex(index); //allow for difficulty to be decreased
+							}
 
-														 //have to check if puzzle is correct (only when allowed by efficiency) and if true, disable buttonListener
-														 if (usrSudokuArr.canCheck()) {
-															 usrSudokuArr.checkPuzzle(v, btnArr);
-														 }
+						});
 
-														 Log.d( "highlight", "marked rectArr (in ButtonListener) as selected at: " + currentRectColoured.getRow() + ", " + currentRectColoured.getColumn() );
+						@Override
+						public boolean onTouch(View v, MotionEvent event) {
+							Log.d("TEST", "Raw event: " + event.getAction() + ", (" + event.getRawX() + ", " + event.getRawY() + ")");
 
-													 }
-
-													 //debug
-													 usrSudokuArr.printCurrent( );
-												 }
-											 }
-
-				);
+							gestureDetector.onTouchEvent(event);
+                            Hint.setBackgroundColor(Color.TRANSPARENT);
+                            Hint.setText("");
+							return true;
+						}
+					});
 			}
 		}
 	}
